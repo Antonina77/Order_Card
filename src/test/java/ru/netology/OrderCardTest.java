196ad26e62a412a2bf4dac759f36f6d208dcd441
Antonina <adrianery94@mail.ru>
2021-01-25 11:19:52 +0300

    add Test invalid commit

--- src/test/java/ru/netology/OrderCardTest.java
+++ src/test/java/ru/netology/OrderCardTest.java
@@ -24,4 +24,36 @@ public class OrderCardTest {
         $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
 
+    @Test
+    public void shouldReturnErrorIfInvalidName() {
+
+        $(".form_theme_alfa-on-white");
+        $("[data-test-id=name] input").setValue("Andrey");
+        $("[data-test-id=phone] input").setValue("+79101234567");
+        $("[data-test-id=agreement]").click();
+        $("[type=button]").click();
+        $(".input_type_text .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+
+    @Test
+    public void shouldReturnErrorIfInvalidTel() {
+
+        $(".form_theme_alfa-on-white");
+        $("[data-test-id=name] input").setValue("Новикова Ирина");
+        $("[data-test-id=phone] input").setValue("4843277");
+        $("[data-test-id=agreement]").click();
+        $("[type=button]").click();
+        $(".input_type_tel .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+
+    }
+
+    @Test
+    public void shouldReturnErrorIfFieldNameIsEmpty() {
+        $(".form_theme_alfa-on-white");
+        $("[data-test-id=name] input").setValue("");
+        $("[data-test-id=phone] input").setValue("+79101234567");
+        $("[data-test-id=agreement]").click();
+        $("[type=button]").click();
+        $(".input_type_text .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+    }
 }
